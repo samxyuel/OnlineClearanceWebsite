0594cc4c8b092772d80282ca5257aed20261ea1e
samxyuel <ms.a.smllcsn@outlook.com>
2025-11-20 21:26:11 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Online Clearance Website - Entry Point
+ * Redirects to the login page for deployment
+ */
+
+// Redirect to the login page
+header('Location: pages/auth/login.php');
+exit();
+
